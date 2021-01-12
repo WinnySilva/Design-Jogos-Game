7e7c6ee3e051484eafe053f802c13b1762b276cd
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-01-11 21:18:19 -0300

    Proof of Concept for rocket movement

---
+++ Assets/RocketSpriteController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketSpriteController : MonoBehaviour
+{
+    public GameObject _leftThruster;
+    public GameObject _rightThruster;
+
+    public float velocity;
+
+    public Rigidbody2D _rbLeft;
+    public Rigidbody2D _rbRight;
+
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+        // _rbLeft.AddForce()
+        var newVelocity = transform.TransformDirection(Vector2.up) * velocity;
+
+        Debug.DrawRay(_rbLeft.position, newVelocity, Color.red);
+        Debug.DrawRay(_rbRight.position, newVelocity, Color.red);
+
+        _rbLeft.velocity = newVelocity;
+        _rbRight.velocity = newVelocity;
+    }
+}
